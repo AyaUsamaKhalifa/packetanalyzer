f9865d140d99c5a7acfeaaed06b7b6f9f3f69a9e
AMahdy98 <ahmadmahdy3098@gmail.com>
2023-10-06 00:19:20 +0300

    Packet Parser Implementation

--- libs/PacketParser.cpp
+++ libs/PacketParser.cpp
@@ -1,24 +1,65 @@
 #include "PacketParser.h"
+#include <format>
 
-PacketParser::PacketParser()
+#define ECPRI_TYPE "AEFE"
+
+PacketParser::PacketParser(std::string line)
 {
+    ParsePacket(line);
 }
 
 std::string PacketParser::ParsePacket(std::string packet)
 {
-    return "";
+    // Check type of ethernet packet
+    if (IseCPRIPacket(packet.substr(((20 * 2) - 1), (2 * 2)))) // Type field is bytes number 21:23 (characters 41:45)
+    {
+        // Create eCPRI packet
+        CurrentDataPacket = new eCPRIPacket();
+
+        // Set data
+        CurrentDataPacket->SetDataField(PacketDataFields::CRC, packet.substr((packet.size() - (4 * 2)), (4 * 2))); // Last 4 bytes
+        CurrentDataPacket->SetDataField(PacketDataFields::DestinationAddress, packet.substr(((8 * 2) - 1), (6 * 2))); // Bytes 9:14
+        CurrentDataPacket->SetDataField(PacketDataFields::MessageType, packet.substr(((23 * 2) - 1), (1 * 2)));       // Byte 24
+        CurrentDataPacket->SetDataField(PacketDataFields::PayloadSize, packet.substr(((24 * 2) - 1), (2 * 2)));       // Bytes 25:26
+        CurrentDataPacket->SetDataField(PacketDataFields::ProtocolVersion, packet.substr(((22 * 2) - 1), (1 * 1)));     // Left Nibble of Byte 23
+        CurrentDataPacket->SetDataField(PacketDataFields::RTCID, packet.substr(((26 * 2) - 1), (2 * 2)));         // Bytes 27:28
+        CurrentDataPacket->SetDataField(PacketDataFields::SequenceID, packet.substr(((28 * 2) - 1), (2 * 2)));    // Bytes 29:30
+        CurrentDataPacket->SetDataField(PacketDataFields::SourceAddress, packet.substr(((14 * 2) - 1), (6 * 2))); // Bytes 15:20
+        CurrentDataPacket->SetDataField(PacketDataFields::Type, packet.substr(((20 * 2) - 1), (2 * 2)));        // Bytes 21:23
+
+        // get last bit in byte 23
+        int byte_23 = std::stoi(packet.substr(((22 * 2) - 1), (1 * 1)), nullptr, 16);
+        std::string last_bit = std::format("{:x}", (byte_23 % 2)); // int to hex string
+        CurrentDataPacket->SetDataField(PacketDataFields::ConcatenationIndicator, last_bit);    // Last bit of Byte 23
+    }
+    else
+    {
+        // Create Raw packet
+        CurrentDataPacket = new RawEthernetPacket();
+
+        // Set Data
+        CurrentDataPacket->SetDataField(PacketDataFields::CRC, packet.substr((packet.size() - (4 * 2)), (4 * 2))); // Last 4 bytes
+        CurrentDataPacket->SetDataField(PacketDataFields::DestinationAddress, packet.substr(((8 * 2) - 1), (6 * 2))); // Bytes 9:14
+        CurrentDataPacket->SetDataField(PacketDataFields::SourceAddress, packet.substr(((14 * 2) - 1), (6 * 2))); // Bytes 15:20
+        CurrentDataPacket->SetDataField(PacketDataFields::Type, packet.substr(((20 * 2) - 1), (2 * 2)));        // Bytes 21:23
+    }
+
+    DataFormatterVisitor formattedPacketVisitor;
+
+    return GetFormattedData(&formattedPacketVisitor);
 }
 
-std::string PacketParser::GetFormattedData()
+std::string PacketParser::GetFormattedData(DataFormatterVisitor* visitor)
 {
-    return "";
+    return CurrentDataPacket->Accept(visitor);
 }
 
 bool PacketParser::IseCPRIPacket(std::string packet)
 {
-    return false;
+    return packet == ECPRI_TYPE;
 }
 
 PacketParser::~PacketParser()
 {
+    delete CurrentDataPacket;
 }
--- libs/PacketParser.h
+++ libs/PacketParser.h
@@ -1,16 +1,23 @@
 #pragma once
 #include "EthernetPacket.h"
+#include "eCPRIPacket.h"
+#include "RawEthernetPacket.h"
+#include "DataFormatterVisitor.h"
 
 #include <string>
 
 class PacketParser
 {
 private:
-    EthernetPacket * CurrentDataPacket;
-public:
-    PacketParser();
-    std::string ParsePacket(std::string packet);
-    std::string GetFormattedData();
+    EthernetPacket *CurrentDataPacket;
+    std::string GetFormattedData(DataFormatterVisitor* visitor);
     bool IseCPRIPacket(std::string packet);
+
+public:
+    PacketParser() = default;
+    PacketParser(std::string);
+
+    std::string ParsePacket(std::string packet);
+
     ~PacketParser();
 };
