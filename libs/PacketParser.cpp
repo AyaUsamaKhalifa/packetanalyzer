1c85a022a34f3c24de50da99efbab7033b85e952
AMahdy98 <ahmadmahdy3098@gmail.com>
2023-10-06 01:02:09 +0300

    fixed string slicing

--- libs/PacketParser.cpp
+++ libs/PacketParser.cpp
@@ -1,5 +1,5 @@
 #include "PacketParser.h"
-#include <format>
+#include <sstream>
 
 #define ECPRI_TYPE "AEFE"
 
@@ -11,26 +11,28 @@ PacketParser::PacketParser(std::string line)
 std::string PacketParser::ParsePacket(std::string packet)
 {
     // Check type of ethernet packet
-    if (IseCPRIPacket(packet.substr(((20 * 2) - 1), (2 * 2)))) // Type field is bytes number 21:23 (characters 41:45)
+    if (IseCPRIPacket(packet.substr(((20 * 2)), (2 * 2)))) // Type field is bytes number 21:23 (characters 41:45)
     {
         // Create eCPRI packet
         CurrentDataPacket = new eCPRIPacket();
 
         // Set data
-        CurrentDataPacket->SetDataField(PacketDataFields::CRC, packet.substr((packet.size() - (4 * 2)), (4 * 2))); // Last 4 bytes
-        CurrentDataPacket->SetDataField(PacketDataFields::DestinationAddress, packet.substr(((8 * 2) - 1), (6 * 2))); // Bytes 9:14
-        CurrentDataPacket->SetDataField(PacketDataFields::MessageType, packet.substr(((23 * 2) - 1), (1 * 2)));       // Byte 24
-        CurrentDataPacket->SetDataField(PacketDataFields::PayloadSize, packet.substr(((24 * 2) - 1), (2 * 2)));       // Bytes 25:26
-        CurrentDataPacket->SetDataField(PacketDataFields::ProtocolVersion, packet.substr(((22 * 2) - 1), (1 * 1)));     // Left Nibble of Byte 23
-        CurrentDataPacket->SetDataField(PacketDataFields::RTCID, packet.substr(((26 * 2) - 1), (2 * 2)));         // Bytes 27:28
-        CurrentDataPacket->SetDataField(PacketDataFields::SequenceID, packet.substr(((28 * 2) - 1), (2 * 2)));    // Bytes 29:30
-        CurrentDataPacket->SetDataField(PacketDataFields::SourceAddress, packet.substr(((14 * 2) - 1), (6 * 2))); // Bytes 15:20
-        CurrentDataPacket->SetDataField(PacketDataFields::Type, packet.substr(((20 * 2) - 1), (2 * 2)));        // Bytes 21:23
+        CurrentDataPacket->SetDataField(PacketDataFields::CRC, packet.substr((packet.size() - 4 * 2), (4 * 2))); // Last 4 bytes
+        CurrentDataPacket->SetDataField(PacketDataFields::DestinationAddress, packet.substr((8 * 2), (6 * 2)));  // Bytes 9:14
+        CurrentDataPacket->SetDataField(PacketDataFields::MessageType, packet.substr((23 * 2), (1 * 2)));        // Byte 24
+        CurrentDataPacket->SetDataField(PacketDataFields::PayloadSize, packet.substr((24 * 2), (2 * 2)));        // Bytes 25:26
+        CurrentDataPacket->SetDataField(PacketDataFields::ProtocolVersion, packet.substr((22 * 2), (1 * 1)));    // Left Nibble of Byte 23
+        CurrentDataPacket->SetDataField(PacketDataFields::RTCID, packet.substr((26 * 2), (2 * 2)));              // Bytes 27:28
+        CurrentDataPacket->SetDataField(PacketDataFields::SequenceID, packet.substr((28 * 2), (2 * 2)));         // Bytes 29:30
+        CurrentDataPacket->SetDataField(PacketDataFields::SourceAddress, packet.substr((14 * 2), (6 * 2)));      // Bytes 15:20
+        CurrentDataPacket->SetDataField(PacketDataFields::Type, packet.substr((20 * 2), (2 * 2)));               // Bytes 21:23
 
         // get last bit in byte 23
         int byte_23 = std::stoi(packet.substr(((22 * 2) - 1), (1 * 1)), nullptr, 16);
-        std::string last_bit = std::format("{:x}", (byte_23 % 2)); // int to hex string
-        CurrentDataPacket->SetDataField(PacketDataFields::ConcatenationIndicator, last_bit);    // Last bit of Byte 23
+        std::stringstream sstring;
+        sstring << std::hex << byte_23 % 2;
+        std::string last_bit = sstring.str();                                                // int to hex string
+        CurrentDataPacket->SetDataField(PacketDataFields::ConcatenationIndicator, last_bit); // Last bit of Byte 23
     }
     else
     {
@@ -38,10 +40,10 @@ std::string PacketParser::ParsePacket(std::string packet)
         CurrentDataPacket = new RawEthernetPacket();
 
         // Set Data
-        CurrentDataPacket->SetDataField(PacketDataFields::CRC, packet.substr((packet.size() - (4 * 2)), (4 * 2))); // Last 4 bytes
+        CurrentDataPacket->SetDataField(PacketDataFields::CRC, packet.substr((packet.size() - (4 * 2)), (4 * 2)));    // Last 4 bytes
         CurrentDataPacket->SetDataField(PacketDataFields::DestinationAddress, packet.substr(((8 * 2) - 1), (6 * 2))); // Bytes 9:14
-        CurrentDataPacket->SetDataField(PacketDataFields::SourceAddress, packet.substr(((14 * 2) - 1), (6 * 2))); // Bytes 15:20
-        CurrentDataPacket->SetDataField(PacketDataFields::Type, packet.substr(((20 * 2) - 1), (2 * 2)));        // Bytes 21:23
+        CurrentDataPacket->SetDataField(PacketDataFields::SourceAddress, packet.substr(((14 * 2) - 1), (6 * 2)));     // Bytes 15:20
+        CurrentDataPacket->SetDataField(PacketDataFields::Type, packet.substr(((20 * 2) - 1), (2 * 2)));              // Bytes 21:23
     }
 
     DataFormatterVisitor formattedPacketVisitor;
@@ -49,7 +51,7 @@ std::string PacketParser::ParsePacket(std::string packet)
     return GetFormattedData(&formattedPacketVisitor);
 }
 
-std::string PacketParser::GetFormattedData(DataFormatterVisitor* visitor)
+std::string PacketParser::GetFormattedData(DataFormatterVisitor *visitor)
 {
     return CurrentDataPacket->Accept(visitor);
 }
